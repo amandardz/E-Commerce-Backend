e5c03156f846bdc5a10b097b37edbee044c7c6d9
amandardz <amandardz15@gmail.com>
2021-05-04 16:14:54 -0500

    modifying category routes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,15 +3,28 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoriesData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  // try {
+  //   const category = await Category.findByPk(req.params.id, {
+  //     include: []
+  //   })
+  }
+);
 
 router.post('/', (req, res) => {
   // create a new category
