c15a40a0bf6dd476cb733b71292b49faadc4055d
amandardz <amandardz15@gmail.com>
2021-05-05 12:31:48 -0500

    modifying functions in category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,12 +19,22 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  // try {
-  //   const category = await Category.findByPk(req.params.id, {
-  //     include: []
-  //   })
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+
+    if(!category) {
+      res.status(404).json({ message: 'No category found with that id'})
+      return;
+    }
+
+    res.status(200).json(tag);
+  } catch(err) {
+    console.log(err);
+    res.staus(500).json(err)
   }
-);
+});
 
 router.post('/', (req, res) => {
   // create a new category
@@ -36,6 +46,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try{
+    const category = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!category) {
+      res.status(404).json({ message: 'No category was delted'})
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
