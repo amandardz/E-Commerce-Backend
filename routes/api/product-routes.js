944a227bad9f19cf79aac166403d368a29663af2
amandardz <amandardz15@gmail.com>
2021-05-05 12:58:07 -0500

    modifying product routes file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -117,18 +117,18 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try{
-    const products = await Product.destroy({
+    const product = await Product.destroy({
       where: {
         id: req.params.id
       }
     });
 
-    if (!products) {
+    if (!product) {
       res.status(404).json({ message: 'No product was deleted'});
-      return
+      return;
     }
 
-    res.status(200).json(products);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
